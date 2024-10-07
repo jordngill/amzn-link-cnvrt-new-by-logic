ef18342857834e65ac702c2e9193336461412f97
jordngill <119168057+jordngill@users.noreply.github.com>
2024-10-07 22:32:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,6 +12,6 @@ DB_NAME = os.environ.get("DB_NAME", "AmazonDB")
 AMAZON_TLD = os.environ.get("AMAZON_TLD", "in")
 AMAZON_TAG = os.getenv("AMAZON_TAG", "gillharyana0e-21")
 FORWARD_CHANNEL_NUMBER = os.getenv("FORWARD_CHANNEL_NUMBER", -1002043723833)
-BOT_TYPE_PUBLIC = os.getenv("BOT_TYPE_PUBLIC", "True").lower() == "False"
+BOT_TYPE_PUBLIC = os.getenv("BOT_TYPE_PUBLIC", "False").lower() == "True"
 COOKIES = os.environ.get("COOKIES", 'ubid-acbin=260-2754458-7012255;x-acbin="8ZtF2K2r7?p?UQUgbSuQMu??lAkXupXz4gSwS@EuQbwyI8SJ?SWPH5h@2nKF3IkT";at-acbin=Atza|IwEBICnZ2qFj4l495oZSgjAeIt82hUBWDVadfSFeUnv7JjBxpwbqkccQj1zHc6HITg7r_783NsHI_iZXsd8xZflXnXZ-9FVtTjRYPvoqYVAoLlf4OqHvC5EJdoD1ZSUIRXO4u9KIolbKoiEahbWYpmIpw4zah88MwpkLtJt6sB3zLO5eubVFsCNWbtU9uQBVyUDnkJNF69bZYjUSetp27OgggsT2WfMZ-3rsP8qH-wauPzpJlg;lc-acbin=en_IN;sess-at-acbin="bXZvMD3QLCiKsTiD518DZyj4bqpIvEYCCdZXOqctOUU=";sst-acbin=Sst1|PQFPQZyK-PfdZkOj6rzHmMcoCWbaW5fz_sb61CDlRvMJHWoE4Z1vessVtn_Jmfn7jnaGr2Cwcj0f6Ri6P-qugvgSoQJagZLXQr38NuZi1EyvlXYcS4CJeIPpjEz9rVbHkDgMaZbmuKDIGDPZS1ghl7CLo-ZIkLtpGdBx80Ci9f5IvENUxSgsr0Y_r-HhZcnVu79PAWk0FDdeYHdKtvdXcm7k_7d-4Qucf6De5WFE_lsit545o5uFQ0bvwiWQShgaEVBxdU7yxQVrSxZiLq1xF3JJvMBR6WEANUqLmUI6Pzve71U')
 BROADCAST_AS_COPY = os.getenv("BROADCAST_AS_COPY", "True").lower() == "true"
